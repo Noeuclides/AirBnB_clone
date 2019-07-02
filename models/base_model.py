59a3d7365657006ea700eb20a27aa977fc6045f2
Noeuclides <euclidesnoeuclides>
2019-07-02 21:19:36 +0000

    [feature] BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -4,11 +4,23 @@ import datetime
 
 class BaseModel:
     ''' Base Class'''
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         ''' init '''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key is '__class__':
+                    continue
+                elif key is 'created_at':
+                    self.created_at = datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                elif key is 'updated_at':
+                    self.updated_at = datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                else:
+                    setattr(self, key, value)
+            
+        else:                
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         ''' str method '''
