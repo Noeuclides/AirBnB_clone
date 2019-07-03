d46a796ff64507fd178e42a6c26a4880be73fed7
ryan hudson <ryan.e.hudson@gmail.com>
2019-07-03 19:17:21 +0000

    [add] other classes

---
+++ models/amenity.py
@@ -0,0 +1,6 @@
+#!/usr/bin/pyhton3
+from .base_model import BaseModel
+
+class User(BaseModel):
+    '''User class'''
+    name = ""
---
+++ models/city.py
@@ -0,0 +1,7 @@
+#!/usr/bin/pyhton3
+from .base_model import BaseModel
+
+class User(BaseModel):
+    '''User class'''
+    state_id = ""
+    name = ""
