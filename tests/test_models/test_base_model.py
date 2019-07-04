2f859e07b6b52a06635f93047c6024e9298be7e6
Noeuclides <euclidesnoeuclides>
2019-07-04 00:22:19 +0000

    [feature] test pep8

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,6 +1,26 @@
 #!/usr/bin/python3
 import unittest
 from models.base_model import BaseModel
+import pep8
+
+
+class TestCodeformat(unittest.TestCase):
+
+    def test_pep8(self):
+        """test pep8"""
+        pep8style = pep8.StyleGuide(quite=True)
+        result = pep8style.check_files([
+            'models/base_model.py', 'models/user.py',
+            'models/review.py', 'models/place.py',
+            'models/city.py', 'models/amenity.py',
+            'tests/test_models/test_engine/test_file_storage.py',
+            'tests/test_models/test_base_model.py',
+            'tests/test_models/test_user.py',
+            'tests/test_models/test_review.py',
+            'tests/test_models/test_place.py',
+            'tests/test_models/test_city.py',
+            'tests/test_models/test_amenity.py'])
+        self.assertEqual(result.total_errors, 0, "Found code style errors")
 
 
 class TestBaseModel(unittest.TestCase):
