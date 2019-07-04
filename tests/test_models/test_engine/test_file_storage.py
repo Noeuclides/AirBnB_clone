e7f2ec9291bbaa5721f8a00fb792fa1fce736d6e
ryan hudson <ryan.e.hudson@gmail.com>
2019-07-04 00:58:59 +0000

    [add] test cases

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,16 +1,35 @@
 #!/usr/bin/python3
 import unittest
+from models import storage
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 
-class TestBaseModel(unittest.TestCase):
+class TestFileStorage(unittest.TestCase):
     def setUp(self):
         self.test1 = BaseModel()
-        self.test1.name = "holberton"
 
-    def test_name(self):
-        self.assertTrue(self.test1.id)
-        self.assertEqual(self.test1.name, "holberton")
+    def test_all(self):
+        stor = storage.all()
+        self.assertTrue(stor)
+
+    def test_new(self):
+        stor1 = str(storage.all())
+        self.test2 = BaseModel()
+        stor2 = str(storage.all())
+        self.assertNotEqual(stor1, stor2)
+
+    def test_save(self):
+        stor3 = str(storage.all())
+        my_model = BaseModel()
+        my_model.name = "Holberton"
+        my_model.my_number = 89
+        my_model.save()
+        stor4 = str(storage.all())
+        self.assertNotEqual(stor3, stor4)
+
+    def tearDown(self):
+        pass
 
 
 if __name__ == '__main__':
